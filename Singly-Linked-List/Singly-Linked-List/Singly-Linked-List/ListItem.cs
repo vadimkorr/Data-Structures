6298b613e0e4122c4d69a7f7094c0bd583b1c969
Vadim <korobeinikovv@mail.ru>
2017-06-08 23:15:03 +0300

    add Singly Linked List

---
+++ Singly-Linked-List/Singly-Linked-List/Singly-Linked-List/ListItem.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Singly_Linked_List
+{
+    class ListItem<T>
+    {
+        public ListItem(T value)
+        {
+            this.Value = value;
+            this.Next = null;
+        }
+        public T Value { get; set; }
+        public ListItem<T> Next { get; set; }
+    }
+}
---
+++ Singly-Linked-List/Singly-Linked-List/Singly-Linked-List/Program.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Singly_Linked_List
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+
+            SinglyLinkedList<string> sll = new SinglyLinkedList<string>();
+
+            try
+            {
+
+                //Adding item
+                for (int i = 0; i < 10; i++)
+                {
+                    sll.Add(i.ToString());
+                }
+
+                //show items
+                foreach (ListItem<string> li in sll)
+                {
+                    Console.Write(li.Value + " ");
+                }
+                Console.WriteLine("\nCount = " + sll.GetCount());
+
+                //get m-th to last item
+                Console.WriteLine("M-th to last: " + sll.GetMToLastItem(6).Value);
+
+                //get i-th to first item
+                for (int i = 0; i < 10; i++)
+                {
+                    Console.WriteLine(sll[i].Value);
+                }
+                Console.WriteLine(sll[10].Value);
+
+            }
+            catch(Exception e)
+            {
+                Console.WriteLine(e.Message);
+            }
+            Console.ReadLine();
+        }
+    }
+}
